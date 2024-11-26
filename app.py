45850d58e7f3c4ab71837e2ad9e97ea02d6d2016
Akrillai <Marks.Legkovskis@warwick.ac.uk>
2024-11-26 00:47:08 +0000

    version update

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ with col1:
     st.image("logo.jpg", width=100)
 
 with col2:
-    st.title('UncertaintyCat | v3.3')
+    st.title('UncertaintyCat | v3.4')
 
 with st.expander("Instructions"):
     st.markdown("""
