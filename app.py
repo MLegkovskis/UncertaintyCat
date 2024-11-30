6f94ff9ec00f53c061fd8ed5f4dbfa7fcb367d24
Mark Legkovskis <55879905+MLegkovskis@users.noreply.github.com>
2024-11-30 13:14:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ with col1:
     st.image("logo.jpg", width=100)
 
 with col2:
-    st.title('UncertaintyCat | v3.7')
+    st.title('UncertaintyCat | v3.8')
 
 with st.expander("Instructions"):
     st.markdown("""
